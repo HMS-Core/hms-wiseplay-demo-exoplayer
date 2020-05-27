c57e40c32ece0318e935070d44266a9be1e1c1ca
leonard-hw <65204583+leonard-hw@users.noreply.github.com>
2020-05-27 10:49:39 +0800

    Update DashManifestParser.java

--- library/dash/src/main/java/com/google/android/exoplayer2/source/dash/manifest/DashManifestParser.java
+++ library/dash/src/main/java/com/google/android/exoplayer2/source/dash/manifest/DashManifestParser.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Modification is made for supporting WisePlay DRM at 2020.5
  */
 package com.google.android.exoplayer2.source.dash.manifest;
 
